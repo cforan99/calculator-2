0cd2cadb329f4f95d2f9c04e51f64a6510af0102
Hackbright Student <info@hackbrightacademy.com>
2015-09-23 17:51:37 -0700

    added instructions and  addition fuctionality

--- calculator.py
+++ calculator.py
@@ -7,6 +7,16 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
-print "Welcome to our calculator. You can perform the following operations on any two numbers: +, -, *, /, square, cube, pow, mod."
+print "Welcome to our calculator!" 
+print "You can perform the following operations on any two numbers: "
+print "+, -, *, /, square, cube, pow, mod."
+print "Please enter the operation followed by the two numbers (ex. pow 3 5)."
 
+calc_input = ""
 
+while calc_input != "q":
+    calc_input = raw_input(">")
+    terms = calc_input.split(" ")
+    if terms[0] == "+":
+        add_answer = add(int(terms[1]), int(terms[2]))
+        print add_answer
