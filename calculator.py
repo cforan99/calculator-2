4135d4ad33da86421bcf7ba590d688615d70cd17
Hackbright Student <info@hackbrightacademy.com>
2015-09-23 18:02:11 -0700

    added all the operations

--- calculator.py
+++ calculator.py
@@ -8,9 +8,10 @@ calculator program yourself in this file.
 from arithmetic import *
 
 print "Welcome to our calculator!" 
-print "You can perform the following operations on any two numbers: "
+print "You can perform the following operations on any two whole numbers: "
 print "+, -, *, /, square, cube, pow, mod."
 print "Please enter the operation followed by the two numbers (ex. pow 3 5)."
+print "Press q to quit."
 
 calc_input = ""
 
@@ -20,3 +21,26 @@ while calc_input != "q":
     if terms[0] == "+":
         add_answer = add(int(terms[1]), int(terms[2]))
         print add_answer
+    elif terms[0] == "-":
+        subtract_answer = subtract(int(terms[1]), int(terms[2]))
+        print subtract_answer
+    elif terms[0] == "*":
+        multiply_answer = multiply(int(terms[1]), int(terms[2]))
+        print multiply_answer
+    elif terms[0] == "/":
+        divide_answer = divide(float(terms[1]), float(terms[2]))
+        print divide_answer
+    elif terms[0] == "square":
+        square_answer = square(int(terms[1]))
+        print square_answer
+    elif terms[0] == "cube":
+        cube_answer = cube(int(terms[1]))
+        print cube_answer
+    elif terms[0] == "pow":
+        pow_answer = power(int(terms[1]), int(terms[2]))
+        print pow_answer 
+    elif terms[0] == "mod":
+        mod_answer = mod(int(terms[1]), int(terms[2]))
+        print mod_answer 
+    else:
+        break   
